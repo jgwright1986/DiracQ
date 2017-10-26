8cd1cc229c25247f55f36d53109a8680d8884e05
jgwright1986 <jgwright1986@gmail.com>
2017-10-26 14:06:50 -0400

    Update DiracQV1.m

--- DiracQ/DiracQV1.m
+++ DiracQ/DiracQV1.m
@@ -2096,6 +2096,9 @@ DeleteOperator[a_]:=Module[{},
 \[Delta][y,x]:=0;
 \[Delta][x,z]:=0;
 \[Delta][z,y]:=0;
+Unprotect[Power];
+\[Delta][i_,j_,OptionsPattern[]]^2=\[Delta][i_,j_,OptionsPattern[]];
+Protect[Power];
 
 
 (* ::Subsection:: *)
